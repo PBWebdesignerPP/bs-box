edf5d6c954807e4f4a5a633ecc2a49d861ae0175
PBWebdesignerPP <paulobarbosawebdesigner@gmail.com>
2026-04-11 15:32:05 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,11 @@
-const CACHE_NAME = "bsbox-v2";
-
 self.addEventListener("install", e => {
-  self.skipWaiting();
-});
+  self.skipWaiting()
+})
 
 self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      );
-    })
-  );
-  self.clients.claim();
-});
+  return self.clients.claim()
+})
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const resClone = res.clone();
-        caches.open(CACHE_NAME).then(cache => {
-          cache.put(e.request, resClone);
-        });
-        return res;
-      })
-      .catch(() => caches.match(e.request))
-  );
-});
+self.addEventListener("fetch", event => {
+  // comportamento padrão (sem cache agressivo)
+})
