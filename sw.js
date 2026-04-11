5a3759c1c013bb07f1742bd0e6b87904e48b93a3
PBWebdesignerPP <paulobarbosawebdesigner@gmail.com>
2026-04-11 16:15:31 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,31 @@
+const CACHE_NAME = "bsbox-v2";
+
 self.addEventListener("install", e => {
-  self.skipWaiting()
-})
+  self.skipWaiting();
+});
 
 self.addEventListener("activate", e => {
-  return self.clients.claim()
-})
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
 
-self.addEventListener("fetch", event => {
-  // comportamento padrão (sem cache agressivo)
-})
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(e.request, resClone);
+        });
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
